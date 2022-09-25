9f4ecec5b78e398b5fa21d81e861654366dce6ce
VictorEdM <thomasmannautor@gmail.com>
2022-09-25 14:18:03 -0300

    Persons addresses endpoint created

--- main.py
+++ main.py
@@ -6,23 +6,76 @@ app = FastAPI()
 persons_db = {
     1: {
         "name": "Carl Scott",
-        "age": 32
+        "age": 32,
+        "addresses": {
+            1: {
+                "street": "Thunder Street",
+                "number": 2
+            },
+            2: {
+                "street": "Bradley Hall",
+                "number": 665
+    
+            },
+            3: {
+                "street": "Locktown",
+                "number": 78
+        
+            }
+    
+        }
     },
     2: {
         "name": "John Bon",
-        "age": 45
+        "age": 45,
+        "addresses": {
+            1: {
+                "street": "St. Gobain",
+                "number": 35
+            },
+            2: {
+                "street": "Milwaukee",
+                "number": 10
+            }
+        }
     },
     3: {
         "name": "Melissa Andrews",
-        "age": 28
+        "age": 28,
+        "addresses": {
+            1: {
+                "street": "Thunder Street",
+                "number": 2
+            }
+        }
     },
     4: {
         "name": "Jim Terry",
-        "age": 22
+        "age": 22,
+        "addresses": {
+            1: {
+                "street": "Ball valley",
+                "number": 478
+            },
+            2: {
+                "street": "Harley Mills",
+                "number": 145
+            }
+        }
     },
     5: {
         "name": "Sarah Bills",
-        "age": 48
+        "age": 48,
+        "addresses": {
+            1: {
+                "street": "Catle bar",
+                "number": 659
+            },
+            2: {
+                "street": "Langdon Paris",
+                "number": 14
+            }
+        }
     }
 }
 
@@ -36,6 +89,7 @@ def home():
         <ul>
             <li>All persons registered: http://localhost:8000/persons</li>
             <li>Individual person based on given numeric ID: http://localhost:8000/persons/id</li>
+            <li>Addresses of a person, given the ID: http://localhost:800/persons/id/addresses</li>
             <li>The API auto generated documentation: http://localhost:8000/docs</li>
         </ul>
     </div>
@@ -54,5 +108,11 @@ def findById(person_id: int):
         "Error": "Person not found",
         "Id": person_id
     })
+
+
+@app.get("/persons/{person_id}/addresses")
+def findAllAddress(person_id: int):
+    return persons_db.get(person_id).get("addresses")
+    
 
     
